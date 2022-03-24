bc9e8fc377978d3bad5042a3571a6a5f671df9fc
philip howley <howley.phil@gmail.com>
2022-03-23 22:35:50 -0400

    Connect sanity and gatsby and add necessary plugins for images

--- gatsby-web/gatsby-config.js
+++ gatsby-web/gatsby-config.js
@@ -1,10 +1,20 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/gatsby-config/
- */
+require('dotenv').config()
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-source-sanity`,
+      options: {
+        projectId: process.env.SANITY_PROJECT_ID,
+        dataset: `production`,
+        watchmode: true,
+        overlayDrafts: !process.env.production,
+        token: process.env.SANITY_TOKEN,
+      },
+    },
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+  ],
 }
